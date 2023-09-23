7fb2fd3371e3a101c7846895900f063abbc65cfc
saif-rehma <144496587+saif-rehma@users.noreply.github.com>
2023-09-23 16:22:45 -0700

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,13 @@
+//Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name
+
+// Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+
+let names: string[]=['Sarmad','Qasim','Zohaib','Adnan'];
+
+//LOp through each name in the array
+
+for (let name of names) {
+    // print a personalize message for each friend
+    console.log(`Hello ${name}, I hope you are good!`)
+}
+
